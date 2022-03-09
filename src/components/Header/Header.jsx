ced6fbda08711cd3f45cc85f245b4e0c28481451
henrique lauar <henriquelauar2002@gmail.com>
2022-03-09 18:43:06 -0300

    Adding header bar styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+// import { Autocomplete } from '@react-google-maps/api'
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search'
+import styles from '../Header/styles.module.css'
 
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <AppBar position="static">
+            <Toolbar className={styles.toolbar}>
+                <Typography variant="h5" className={styles.title}>
+                    Travel Advisor
+                </Typography>
+                <Box className={styles.searchBox}>
+                    <Typography variant="h6" className={styles.title}>
+                        Explore new places
+                    </Typography>
+                    {/* <Autocomplete> */}
+                        <div className={styles.searchDiv}>
+                            <div>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..."/>
+                        </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
